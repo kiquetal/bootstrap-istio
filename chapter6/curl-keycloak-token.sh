08a59c9d5661a897a5dfb807f616908edb2b5609
kiquetal <kiquetal@gmail.com>
2024-02-16 23:27:00 -0300

    using new cluster with keycloak on istio

---
+++ chapter6/curl-keycloak-token.sh
@@ -0,0 +1,6 @@
+curl --location --request POST 'http://192.168.76.2:30946/realms/sandbox/protocol/openid-connect/token' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--data-urlencode 'grant_type=client_credentials' \
+--data-urlencode 'client_id=mi-tienda-gt' \
+--data-urlencode 'client_secret=uw3U85kvjhcjxwM3W9OhXGE1v8yMCYWo'
+
